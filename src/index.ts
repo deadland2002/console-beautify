521ebef21619e196de10e42d0f47e9ce0373cb95
satvik <satvikshukla453@gmail.com>
2024-04-08 17:56:30 +0530

    auto commit at 1712579190

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import AsyncLog from "./AsyncLog/AsyncLog";
 import SimpleLog from "./SimpleLog/SimpleLog";
 import FunctionLog from "./FunctionLog/FuntionLog";
 
-
 export {AsyncLog, SimpleLog , FunctionLog}
