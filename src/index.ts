c5c8bf56cd075867c1704ac3d288ea5331258d2a
satvik <satvikshukla453@gmail.com>
2024-04-08 17:11:21 +0530

    added space for testing

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import AsyncLog from "./AsyncLog/AsyncLog";
 import SimpleLog from "./SimpleLog/SimpleLog";
 import FunctionLog from "./FunctionLog/FuntionLog";
 
+
 export {AsyncLog, SimpleLog , FunctionLog}
